225eba8ec27bd83959024c7acb768276c563b6c3
cjkelley <ckelley557@gmail.com>
2015-11-08 14:07:26 -0600

    tree final

--- tree.js
+++ tree.js
@@ -17,36 +17,29 @@
         .append("g")
         .attr("transform", "translate(70,0)");
 
+        //choosing json data
         d3.json("soccer.json", function(error, root) {
             if (error) throw error;
             
+            //setting tree root, and binding data to each node
             nodes = tree.nodes(root),
                 links = tree.links(nodes);
-
-//            data.forEach(function(d) {
-//                var home = d.team1;
-//                var away = d.team2;
-//                var homeScore = d.t1Score;
-//                var awayScore = d.t2Score;
-//                var homeResult = d.t1Result;
-//                var awayResult = d.t2Result;
-//                var location = d.location;
-//                var date = d.date;
-//                var round = d.round;
-//            });
         
+            //setting paths between nodes
             var link = svg.selectAll(".link")
             .data(links)
             .enter()
             .append("g")
             .attr("class", "link");
 
+            //colloring each path blue
             link.append("path")
                 .attr("fill", "none")
                 .attr("stroke", "blue")
                 .attr("stroke-width", "1.5px")
                 .attr("d", diagonal);
 
+            //adding text to each node
             link.append("text")
                 .attr("font-family", "Arial, Helvetica, sans-serif")
                 .attr("fill", "Black")
@@ -55,7 +48,7 @@
                 return "translate(" +
                     ((d.source.y + d.target.y)/2) + "," + 
                     ((d.source.x + d.target.x)/2) + ")";
-            })   
+            })  
                 .attr("dy", ".35em")
                 .attr("text-anchor", "middle")
                 .text(function(d) {
@@ -63,6 +56,7 @@
                 return d.target.rule;
             });
 
+            //set node locaton
             var node = svg.selectAll(".node")
             .data(nodes)
             .enter()
@@ -72,9 +66,12 @@
                 return "translate(" + d.y + "," + d.x + ")";
             });
 
+            //set node shape
             node.append("circle")
                 .attr("r", 4.5);
             
+            //modifiying the text location to center the text in the middle of node
+            //and underneath
             node.append("text")
                 .attr("dx", function (d) {
                 return d.children ? -8 : 8;
@@ -84,29 +81,39 @@
                 .text(function (d) {
                 return d.team1 + " vs " + d.team2;
             });
+            
+            //click listener
+            node.on("click", function(d){
+                    update(d);
+            });
         });
 
-//        var data = updateData(function() {
-//        
-//            d3.json("soccer.json", function(error, root) {
-//                if (error) throw error;
-//            
-//                home = d.team1;
-//                away = d.team2;
-//                homeScore = d.t1Score;
-//                awayScore = d.t2Score;
-//                homeResult = d.t1Result;
-//                awayResult = d.t2Result;
-//                location = d.location;
-//                date = d.date;
-//                round = d.round;
-//            
-//                
-//            }); 
-//        
-//        });     
-//    
-//        var results = matchResults(function() {
-//            updateData();
-//        }); 
-        
+    
+        //update function
+        var update = function(d) {
+              
+            console.log(d);  
+
+            //set variable values
+            var home = d.team1;
+            var away = d.team2;
+            var homeScore = d.t1Score;
+            var awayScore = d.t2Score;
+            var homeResult = d.t1Result;
+            var awayResult = d.t2Result;
+            var location = d.location;
+            var date = d.date;
+            var round = d.round;
+            
+            //apend the values to the Results div
+            var results = d3.select("#results").html("");
+            results.append("p").text("Home Team: " + home);
+            results.append("p").text("Away Team: " + away);
+            results.append("p").text("Home Score: " + homeScore);
+            results.append("p").text("Away Score: " + awayScore);
+            results.append("p").text("Home Result: " + homeResult);
+            results.append("p").text("Away Result: " + awayResult);
+            results.append("p").text("Location: " + location);
+            results.append("p").text("Round: " + round);
+
+        };     
